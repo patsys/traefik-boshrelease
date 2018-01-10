02f22129b8e0acc06aaab61801f4d5dc3f44adad
Benjamin Gandon <benjamin@gandon.org>
2018-01-10 20:25:40 +0100

    Fix the 'add-blob' script

--- scripts/add-blobs.sh
+++ scripts/add-blobs.sh
@@ -5,15 +5,14 @@ set -ex
 version=1.4.6
 
 if [ ! -f "traefik-${version}_linux-amd64" -a ! -f "traefik-${version}_linux-amd64.gz" ]; then
-    curl -L https://github.com/containous/traefik/releases/download/v$version/traefik_linux-amd64 \
-        -o traefik-${version}_linux-amd64
+    curl -L "https://github.com/containous/traefik/releases/download/v$version/traefik_linux-amd64" \
+        -o "traefik-${version}_linux-amd64"
+    echo "ce46543db09c8b885e71d8a3b89251ba4b9e1bee56f238cc05651c9eb29e32b2  traefik-${version}_linux-amd64" \
+        | shasum -a 256 --check
 fi
-
-echo "ce46543db09c8b885e71d8a3b89251ba4b9e1bee56f238cc05651c9eb29e32b2  traefik-${version}_linux-amd64" \
-    | shasum -a 256 --check
 
 if [ -f "traefik-${version}_linux-amd64" -a ! -f "traefik-${version}_linux-amd64.gz" ]; then
-    gzip -9 traefik-${version}_linux-amd64
+    gzip -9 "traefik-${version}_linux-amd64"
 fi
 
-bosh add-blob traefik-${version}_linux-amd64.gz traefik/traefik-${version}_linux-amd64.gz
+bosh add-blob "traefik-${version}_linux-amd64.gz" "traefik/traefik-${version}_linux-amd64.gz"
