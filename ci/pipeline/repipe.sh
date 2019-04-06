2ef2d95cc52b9d3464369d5529f2e158fbbd77e7
Benjamin Gandon <benjamin@gstack.io>
2019-04-06 22:21:04 +0200

    Have the repipe script support being called from any current working directory

--- ci/pipeline/repipe.sh
+++ ci/pipeline/repipe.sh
@@ -2,7 +2,13 @@
 
 set -eo pipefail -x
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+pushd "${SCRIPT_DIR}" > /dev/null
+
 fly -t gk \
     set-pipeline -p traefik-release \
     -c traefik-pipeline.yml \
     -l config.yml -l secrets.yml
+
+popd > /dev/null
