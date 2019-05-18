1464dc8f91760b50e714d672e92840b8440ad1c9
Benjamin Gandon <benjamin@gstack.io>
2019-05-18 06:59:17 +0200

    Implement a first basic version of smoke test errand + Have the rename deployment/network ops file be the default

--- scripts/add-blobs.sh
+++ scripts/add-blobs.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eo pipefail -u -x
 
-version=1.6.5
+version=1.7.11
+sha256=68332497361cbb694545833c7efb1f569db48a0e5265fad7ca75c91b900c3faa
 
-if [ ! -f "traefik-${version}_linux-amd64" -a ! -f "traefik-${version}_linux-amd64.gz" ]; then
+if [[ ! -f "traefik-${version}_linux-amd64" && ! -f "traefik-${version}_linux-amd64.gz" ]]; then
     curl -L "https://github.com/containous/traefik/releases/download/v$version/traefik_linux-amd64" \
         -o "traefik-${version}_linux-amd64"
-    echo "9e77c7664e316953e3f5463c323dffeeecbb35d0b1db7fb49f52e1d9464ca193  traefik-${version}_linux-amd64" \
-        | shasum -a 256 --check
+    shasum -a 256 --check <<< "${sha256}  traefik-${version}_linux-amd64"
 fi
 
-if [ -f "traefik-${version}_linux-amd64" -a ! -f "traefik-${version}_linux-amd64.gz" ]; then
+if [[ -f "traefik-${version}_linux-amd64" && ! -f "traefik-${version}_linux-amd64.gz" ]]; then
     gzip -9 "traefik-${version}_linux-amd64"
 fi
 
-bosh add-blob "traefik-${version}_linux-amd64.gz" "traefik/traefik-${version}_linux-amd64.gz"
+blob_path="traefik/traefik-${version}_linux-amd64.gz"
+set +o pipefail
+if ! bosh blobs | grep -q "${blob_path}"; then
+    bosh add-blob "traefik-${version}_linux-amd64.gz" "${blob_path}"
+fi
