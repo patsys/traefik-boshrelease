7dea4d30069e247f423671e5233cc4237925451b
Benjamin Gandon <benjamin@gstack.io>
2019-06-09 13:51:40 +0200

    Modify pipeline name, to circumvent an history rewrite in traefik repository

--- ci/pipeline/repipe.sh
+++ ci/pipeline/repipe.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 pushd "${SCRIPT_DIR}" > /dev/null
 
 fly -t gk \
-    set-pipeline -p traefik-release \
+    set-pipeline -p traefik-bump \
     -c traefik-pipeline.yml \
     -l config.yml -l secrets.yml
 
