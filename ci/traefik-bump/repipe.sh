548705d33f502ce6f8a7db6952ab7528f160b124
Benjamin Gandon <benjamin@gstack.io>
2019-10-03 22:41:05 +0200

    Rename directory and pipeline definition file for the 'traefik-bump' pipeline

--- ci/traefik-bump/repipe.sh
+++ ci/traefik-bump/repipe.sh
@@ -6,9 +6,9 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 pushd "${SCRIPT_DIR}" > /dev/null
 
-fly -t gk \
-    set-pipeline -p traefik-bump \
-    -c traefik-pipeline.yml \
-    -l config.yml -l secrets.yml
+fly -t "gk" \
+    set-pipeline -p "traefik-bump" \
+    -c "traefik-bump-pipeline.yml" \
+    -l "config.yml" -l "secrets.yml"
 
 popd > /dev/null
